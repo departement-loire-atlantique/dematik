601b2edae74cc00efeaa87ca4964be594440c604
laniauj <65405932+laniauj@users.noreply.github.com>
2020-06-29 15:00:40 +0200

    Update condition_parser.py

--- dematik/condition/condition_parser.py
+++ dematik/condition/condition_parser.py
@@ -87,8 +87,6 @@ class ConditionParser:
     def parse(self, text):
         clean_text = ' '.join(text.split())
         tokens = [token for token in self.tokenize(clean_text) ]
-        # print("********************************")
-        # print(tokens)
         
         # Merge tokens until nothing could be merged anymore
         last_token_count = len(tokens) + 1
@@ -98,31 +96,19 @@ class ConditionParser:
                 new_merge = True
                 while new_merge :
                     tokens,new_merge = self.merge_using_condition(condition_class, tokens)
-                    # print("-------")
-                    # print(condition_class)
-                    # print(tokens)
-                    # print("-------")
 
         if len(tokens) != 1:
             unknowns = [token.value + '(' + token.type + ')' for token in tokens if not token.merged]
             if unknowns: 
-                # for t in tokens:
-                    # print(t.type, t.value)
-                    # print("----")
                 raise Exception("Les termes suivants ne sont pas encore supportés dans les conditions : %s" % ', '.join(unknowns))
             else:
-                # for token in tokens:
-                    # print(token)
                 raise Exception("La phrase n'est pas supportée")
-        # print(tokens[0])
-        # print("********************************")
         
         return tokens[0].value
 
     # Merge tokens using a condition
     def merge_using_condition(self, condition_class, tokens):
         sentences = condition_class.sentences
-        # new_tokens = []
         for sentence in sentences:
             sentence_index = 0
             for i, token in enumerate(tokens):
@@ -132,22 +118,8 @@ class ConditionParser:
                         # A valid sentense for this condition match => Merge
                         c = condition_class(tokens[i-sentence_index+1:i+1])
                         condition_token = [Token(type=c.getType(), value=c, merged=True)]
-                        # new_tokens += [[i-sentence_index+1,len(sentence), condition_token]]
-                        # sentence_index = 0
-                        # print(tokens[:i-sentence_index+1] + condition_token + tokens[i+1:])
                         return tokens[:i-sentence_index+1] + condition_token + tokens[i+1:],True
                 else:
                    sentence_index = 0
-        # No any sentense for this condition match => Do nothing
-        
-        # token_delete = 0
-        # for merge in new_tokens :
-            # first_token = merge[0]-token_delete
-            # last_token = merge[0]+merge[1]- token_delete
-            # tokens = tokens[:first_token] + merge[2] + tokens[last_token:]
-            # token_delete += merge[1] - 1
-        # print("*************")
-        # print(condition_class)
-        # print(tokens)
-        # print("*************")
+                
         return tokens,False
